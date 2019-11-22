c7e16617d1d560530c415c9680c50f30c07b3447
ajaygummuluri <47699065+ajaygummuluri@users.noreply.github.com>
2019-11-22 13:24:56 +0530

    Update Circle_assembly.s

--- Class_Test/Circle_assembly.s
+++ Class_Test/Circle_assembly.s
@@ -1,115 +1,126 @@
-
-     THUMB
-	  AREA     factorial, CODE, READONLY
-	  IMPORT printMsg
+	THUMB
+	  AREA     pixels, CODE, READONLY
       EXPORT __main
+	  IMPORT printMsg
+	  IMPORT printMsg2p
+	  IMPORT printMsg4p
       ENTRY 
-__main  FUNCTION	
-; IGNORE THIS PART 
+__main  FUNCTION
+		MOV R3,#91
+		MOV R4,#1
 
-;Degree to radian conversion
-      
-       VLDR.F32 s31, =15
-       VLDR.F32 s30, =180	   
-	   VLDR.F32 s21, =-180
-loop1  BL sinecosine
-       VADD.F32 s21,s21,s31
+		VLDR.F32 S22,=0 ; initial angle
+		VLDR.F32 S1,=4 ; increment
+		
+		VLDR.F32 S23,=200; radius
+		
+	;Degrees to Radian Conversion
+		
+		VLDR.F32 S31, =3.14159 ; storing Pi value 
+		VLDR.F32 S30, =180 ; 
+		VDIV.F32 S29,S31,S30 ; Pi/180
+		
+		VLDR.F32 S4,=2 ; to calculate 2*i, ((2*i)+1) and ((2*i)-1)
+		
+		VLDR.F32 S18,=320; x shift
+		VLDR.F32 S19,=240; y shift
 
-
-	   VLDR.F32 s29, =200    ; radius
-
-
-       VMUL.F32 s28,s29,s7    ; x = rcosteta
-
-
-       VMUL.F32 s29,s29,s5   ; y = rsinteta
-
-
-
-       VLDR.F32 s27, =240   ; origin X point
-       VLDR.F32 s26, =320    ; origin Y point
-	   
-      VADD.F32 s28,s28,s27
-      VADD.F32 s29,s29,s26
+COUNT	CMP R4,R3			;Compare 'i' and 'n'
+		BLE Angle
+		B stop
 
 
-      VCVT.U32.F32 s28,s28
-    ;   VCVT.U32.F32 s29,s29
+Angle	VMUL.F32 S2,S2,S29 ; (pi/180)*angle - given angle is converted to radians 
 
-      VMOV.F32 R0,S28
-	  
-	 ;  VMOV.F32 R0,S29
-       BL printMsg	 ; Refer to ARM Procedure calling standards.
-   
-       VCMP.F32 s21,s30
-	   vmrs APSR_nzcv,FPSCR
-	   BLE loop1
+	;Every term has to be multiplied by x^2 for next iteration 
+		VMUL.F32 S3,S2,S2 ; x*x
+		
+		;VCVT.U32.F32 S22,S22
+		VMOV.F32 R0,S22
+		;BL printMsg	 
+		
+		BL SIN              ;calling sin subroutine
+		VMUL.F32 S24,S0,S23
+		VADD.F32 S24,S24,S18
+		VCVT.U32.F32 S24,S24
+		VMOV.F32 R0,S24	
+		;BL printMsg	 ; printing 
+		
+		BL COS            ;calling cos subroutine
+		VMUL.F32 S26,S20,S23
+		VADD.F32 S26,S26,S19
+		VCVT.U32.F32 S26,S26
+		VMOV.F32 R1,S26	
+		BL printMsg2p	 ; printing r(sin(theta)) and r(cos(theta))
+		
+		VADD.F32 S22,S22,S1	;
+		VMOV.F32 S2,S22;
+		ADD R4,R4,#1
+		B COUNT
+		
+		
 
-	
-sinecosine 
+SIN		MOV R5,#10 ; No of terms in a series 
+		MOV R1,#1 ; counting variable n (To repeat the loop)
 
-       VLDR.F32 s22, =3.14159
-       VLDR.F32 s23, =180
-       VDIV.F32 s24,s22,s23;  (pi/180)
-       VMUL.F32 s0,s24,s21	
-	
-	
-	
+		VMOV.F32 S11,S2 ; Using S11 to store the intermediate result of sinx
+		VMOV.F32 S0,S2	; Sinx result is stored in s0 register 
+		
+		VLDR.F32 S5,=1; i value
+		VLDR.F32 S6,=1; increment value
+		
 
-      MOV R0,#0x00000014
-	 
-
-      VLDR.F32 s2, =1    ;iterations	count i
-	  MOV R1,#0x00000001
-	  
-	  VLDR.F32 s3, =1     ;increment i
-	  MOV R2,#0x00000001
- 
-	  VMOV.F32 s4, s0
- 
-	  VMOV.F32 s5, s0   ; sin value stored in s5
-	   
-	  VLDR.F32 s6, =1    ; cos value stored in s6
-	 
-	  VLDR.F32 s7, =1	  
-
-   VMUL.F32 s8,s0,s0
-
-	  VLDR.F32 s9, =2
-
-Loop  
-
-
-
-      VMUL.F32 s10,s2,s9
-      VADD.F32 s11,s10,s3
-      VMUL.F32 s12,s10,s11
-      VDIV.F32 s13,s8,s12
-      
-      VSUB.F32 s14,s10,s3
-      VMUL.F32 s15,s10,s14 
-      
-      VDIV.F32 s16,s8,s15
-      
-     	
-      VNMUL.F32 s4,s4,s13   ;Product_sin s4
-      
-      	
-      VNMUL.F32 s6,s6,s16   ;Product_cos s6
-      
-      
-      VADD.F32 s5,s5,s4    ;sine_result
-      
-     
-      VADD.F32 s7,s7,s6     ;cos_result
-
-      VADD.F32 s2,s2,s3
-	  ADD R1,R1,R2
-	  CMP R1,R0	
-	  BLT Loop
-      BX lr
-
-	
-stop    B stop ; stop program
-      ENDFUNC
-      END 
+COUNT1  CMP R1,R5			;Compare 'i' and 'n' 
+		BLE LOOP1			;if i < n goto LOOP
+		BX LR
+			
+LOOP1   VMUL.F32 S7,S4,S5	; 2*i
+		VADD.F32 S8,S7,S6	; (2*i)+1
+		
+		;Calculating next term for sinx 
+		VNMUL.F32 S9,S11,S3	; 
+		VDIV.F32 S10,S9,S7
+		VDIV.F32 S11,S10,S8
+		
+		;Adding the new term to the previous result 
+		VADD.F32 S0,S0,S11	; (for sinx)
+		
+		;incrementing i value
+		VADD.F32 S5,S5,S6
+		
+		ADD R1,R1,#1		;Increment the counter variable 'i'
+		B COUNT1				;Again goto comparision
+		
+COS		MOV R5,#10 ; No of terms in a series 
+		MOV R1,#1 ; counting variable n (To repeat the loop)
+		
+		VLDR.F32 S20,=1 ; Cosx  result is stored in s20 register
+		VLDR.F32 S15,=1	; Using S15 to store the intermediate result of cosx
+		
+		VLDR.F32 S5,=1; i value
+		VLDR.F32 S6,=1; increment value
+		
+COUNT2	CMP R1,R5			;Compare 'i' and 'n' 
+		BLE LOOP2			;if i < n goto LOOP
+		BX LR
+			
+LOOP2   VMUL.F32 S7,S4,S5	; 2*i
+		VSUB.F32 S12,S7,S6	; (2*i)-1
+		
+		;Calculating next term for sinx 
+		VNMUL.F32 S13,S15,S3; 
+		VDIV.F32 S14,S13,S7
+		VDIV.F32 S15,S14,S12
+		
+		;Adding the new term to the previous result 
+		VADD.F32 S20,S20,S15; (for cosx)
+		
+		;incrementing i value
+		VADD.F32 S5,S5,S6
+		
+		ADD R1,R1,#1		;Increment the counter variable 'i'
+		B COUNT2			;Again goto comparision
+			
+stop    B stop
+		ENDFUNC
+		END 
